095197a5dc8cf5bebe2d396e2458beb419b8bf99
ZyaxitronED <33060742+ZyaxitronED@users.noreply.github.com>
2017-12-01 19:24:04 +0000

    failed random colour generation for background included(keeping for further development)

--- sketch.js
+++ sketch.js
@@ -14,8 +14,14 @@ function setup() {
   textSize(120);
 }
 
+var value = 0;
+
 function draw() {
+
   background(0);
+
+  fill(value);
+  rect(0, 0, 841, 594);
 
   //rotate(random(HALF_PI), random(HALF_PI), random(HALF_PI))
   //rotate(PI/50);
@@ -23,25 +29,25 @@ function draw() {
 //This 'fill' chooses 1 random colour for all of the shapes
 fill(random(255), random(255), random(255));
 
+//This section creates randomly generated circles at y=290
   for (var i = 0; i < 150; i++) {
-    //push()
   	ellipse(random(width), 290, random(150), random(150));
-//This 'fill' allows all of the shapes to have their own random colour
+
+//This 'fill' allows all of the shapes within this loop to have their own random colour
 //fill(random(255), random(255), random(255))
-  //  if(i<100){
-    //  rect(random(width), random(width), random(150), random(150))
-    //  pop()
-    //}
   }
 
+//This section creates randomly generated rectangles at y=0
   for (var i = 0; i < 150; i++) {
     rect(random(width), 0, random(150), random(150));
   }
 
+//This section creates randomly generated rectangles at y=594
   for (var i = 0; i < 150; i++) {
     rect(random(width), 594, random(-150), random(-150));
   }
 
+//This section creates randomly generated lines at y=841
   for (var i = 0; i < 300; i++) {
     var r = random(255);
     stroke(0);
@@ -49,6 +55,13 @@ fill(random(255), random(255), random(255));
   }
 }
 
+function mousePressed() {
+  if (value == 0) {
+    value = random(255);
+  } else {
+    value = 0;
+  }
+}
 
 //https://experiments.withgoogle.com/chrome?tag=Generative
 //Project 1: http://weareinstrument.com/kaleidoscope/kaleid.html
