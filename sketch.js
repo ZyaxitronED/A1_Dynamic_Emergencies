5fbcc9a670b999b5308e20a9c93738db668b1260
ZyaxitronED <33060742+ZyaxitronED@users.noreply.github.com>
2017-12-03 12:40:55 +0000

    updated comments

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@
 let r = 255;
 let g = 255;
 let b = 255;
+//------------------------------
 
 //Within this function is everything that will be ran once before the 'draw' function starts working
 function setup() {
@@ -14,11 +15,13 @@ function setup() {
   frameRate(13);
   //noLoop();
 }
+//---------------------------
 
 function draw() {
 
 //The 'rgba' value means that the first three values determine the colour, and the final one adds certain transparency
   background('rgba(0, 0, 0, 0)');
+//---------------------------------
 
 //To enable any of these, remove the '//' before the code. These will enable certain rotations
   //rotate(random(HALF_PI), random(HALF_PI), random(HALF_PI))
@@ -26,21 +29,25 @@ function draw() {
 
 //This 'fill' chooses 1 random colour for all of the shapes below it, unless specified otherwise
 fill(random(255), random(255), random(255));
+//----------------------------------------------
 
 //This section creates randomly generated circles at y=290
   for (var i = 0; i < 150; i++) {
   	ellipse(random(width), 290, random(150), random(150));
   }
+//---------------------------------------------------------
 
 //This section creates randomly generated rectangles at y=0
   for (var i = 0; i < 150; i++) {
     rect(random(width), 0, random(150), random(150));
   }
+//----------------------------------------------------------
 
 //This section creates randomly generated rectangles at y=594
   for (var i = 0; i < 150; i++) {
     rect(random(width), 594, random(-150), random(-150));
   }
+//-----------------------------------------------------------
 
 //This section creates randomly generated lines at y=841
   for (var i = 0; i < 200; i++) {
@@ -48,6 +55,7 @@ fill(random(255), random(255), random(255));
     stroke(0);
     line(random(width), 841, random(width), random(400-420));
   }
+//------------------------------------------------------------
 
 //Changes the RGB values when the mouse is pressed
   if (mouseIsPressed) {
@@ -55,8 +63,9 @@ fill(random(255), random(255), random(255));
     g = random(255);
     b = random(255);
   }
+//--------------------------------------------------
 
-  //This section of code means that randomly generated circles will appear around the curson when it is on the canvas
+//This section of code means that randomly generated circles will appear around the curson when it is on the canvas
   noStroke();
   fill(r, g, b);;
   ellipse(mouseX, mouseY, random(150), random(150));
@@ -69,8 +78,9 @@ fill(random(255), random(255), random(255));
   ellipse(mouseX - 50, mouseY/2, random(150), random(150));
   ellipse(mouseX + 50, mouseY, random(150), random(150));
   ellipse(mouseX + 50, mouseY/2, random(150), random(150));
+//--------------------------------------------------------------
 
-  //This creates a random background colour for each frame
+//This creates a random background colour for each frame
   function random_bg_color() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -82,6 +92,7 @@ fill(random(255), random(255), random(255));
     }
 
 random_bg_color();
+//-----------------------------------------------------------
 
 }
 
